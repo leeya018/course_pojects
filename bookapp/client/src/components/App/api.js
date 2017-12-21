be8bf4caa96a69ed48b1e2f649ca7bfdff5cb331
leeya018 <leeyahav018@gmail.com>
2017-12-21 09:47:31 +0200

    add port and host cause of proxy

--- bookapp/client/src/components/App/api.js
+++ bookapp/client/src/components/App/api.js
@@ -1,6 +1,9 @@
+const PORT = 3005
+const HOST='http://ec2-18-217-200-204.us-east-2.compute.amazonaws.com'
+
 class Api {
   async getData() {
-    let response = await fetch('http://localhost:3001/books');
+    let response = await fetch(`${HOST}:${PORT}/books`);
     return await response.json();
   }
 }
