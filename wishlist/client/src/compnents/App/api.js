998f37a0477945469799c66558b8f30fea748f0c
leeya018 <leeyahav018@gmail.com>
2017-12-25 02:37:26 +0200

    ch host

--- wishlist/client/src/compnents/App/api.js
+++ wishlist/client/src/compnents/App/api.js
@@ -1,5 +1,5 @@
 const PORT =3009
-const HOST='http://ec2-18-217-200-204.us-east-2.compute.amazonaws.com'
+const HOST='http://ec2-13-58-126-247.us-east-2.compute.amazonaws.com'
 // const HOST='http://127.0.0.1'
 
 
