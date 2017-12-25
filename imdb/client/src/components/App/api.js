78dce4704dd3628a07d32875303240c776766b02
leeya018 <leeyahav018@gmail.com>
2017-12-25 02:18:10 +0200

    ch port

--- imdb/client/src/components/App/api.js
+++ imdb/client/src/components/App/api.js
@@ -1,5 +1,5 @@
 const PORT=3003;
-const HOST='http://ec2-18-217-200-204.us-east-2.compute.amazonaws.com'
+const HOST='http://ec2-18-217-212-139.us-east-2.compute.amazonaws.com'
 
 class Api {
     constructor() {
