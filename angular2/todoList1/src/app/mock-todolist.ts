c166378bb9dc85945719143ab23daf45d7c466b7
leeya018 <leeyahav018@gmail.com>
2018-01-10 22:53:08 +0200

    d

--- angular2/todoList1/src/app/mock-todolist.ts
+++ angular2/todoList1/src/app/mock-todolist.ts
@@ -1,9 +1,9 @@
 import { ListItem } from './listItem';
 
 export const ITEMS: ListItem[] = [
-  { id: 1, title: "laundary", time:"20171031" },
-  {id:2,title:"dishes",time:"20180122"},
-  { id: 3, title: "toilet", time: "20171211" },
-  { id: 4, title: "food- take the food from granni", time:"20171031" }
+  { id: 1, title: "laundary", time:"20171031",src:'' },
+  {id:2,title:"dishes",time:"20180122",src:''},
+  { id: 3, title: "toilet", time: "20171211",src:'' },
+  { id: 4, title: "food- take the food from granni", time:"20171031",src:'' }
 
 ]
